be74436bd9dd6cca573114534dcbf97ebec8ab40
IPandragonI <mathysf84pro@gmail.com>
2025-07-09 15:08:56 +0200

    Initial commit

---
+++ src/test/java/esgi/fyc/sso/authserver/AuthserverApplicationTests.java
@@ -0,0 +1,13 @@
+package esgi.fyc.sso.authserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
